3888ce7b6139d6bcfa3c24ab1fec634fdbab6a88
Frank Casanova <frankcasanova.info@gmail.com>
2025-03-31 12:00:02 +0200

    refactor(main_scene): clean up code and improve readability
    - Grouped imports for better organization
    - Removed redundant comments
    - Reordered mob connection logic for clarity

--- squash-the-creeps/rust/src/main_scene.rs
+++ squash-the-creeps/rust/src/main_scene.rs
@@ -2,19 +2,13 @@ use crate::mob;
 use crate::player;
 use crate::scorelabel;
 
-use godot::classes::ColorRect;
-use godot::classes::InputEvent;
-use godot::classes::PathFollow3D;
-use godot::classes::Timer;
+use godot::classes::{ColorRect, InputEvent, PathFollow3D, Timer};
 use godot::prelude::*;
 use rand::Rng;
 
-// Deriving GodotClass makes the class available to Godot.
-// extends Node
 #[derive(GodotClass)]
 #[class(init, base=Node)]
 pub struct MainScene {
-    // export(PackedScene) var mob_scene
     #[export]
     mob_scene: OnEditor<Gd<PackedScene>>,
 
@@ -30,21 +24,17 @@ pub struct MainScene {
 #[godot_api]
 impl INode for MainScene {
     fn ready(&mut self) {
-        // $UserInterface/Retry.hide()
         self.base()
             .get_node_as::<ColorRect>("UserInterface/Retry")
             .hide();
     }
     fn unhandled_input(&mut self, event: Gd<InputEvent>) {
-        // if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
         if event.is_action_pressed("ui_accept")
             && self
                 .base()
                 .get_node_as::<ColorRect>("UserInterface/Retry")
                 .is_visible()
         {
-            // warning-ignore:return_value_discarded
-            // get_tree().reload_current_scene()
             self.base().get_tree().unwrap().reload_current_scene();
         }
     }
@@ -54,42 +44,36 @@ impl MainScene {
     #[func]
     fn on_mob_timer_timeout(&mut self) {
         // Create mob instance
-        // var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
         let mut mob_spawn_location = self
             .base()
             .get_node_as::<PathFollow3D>("SpawnPath/SpawnLocation");
 
         // Choose a random location on the SpawnPath.
         // Set random progress using proper rng
-        // mob_spawn_location.progress_ratio = randf()
         mob_spawn_location.set_progress_ratio(rand::rng().random_range(0.0..=1.0));
 
         // Communicate the spawn location and the player's location to the mob.
-        // var player_position = $Player.position
         let player_position = self
             .base()
             .get_node_as::<player::Player>("Player")
             .get_position();
 
-        // var mob = mob_scene.instantiate()
         let mut mob = self.mob_scene.instantiate_as::<mob::Mob>();
 
-        // mob.initialize(mob_spawn_location.position, player_position)
         mob.bind_mut()
             .initialize(mob_spawn_location.get_position(), player_position);
 
-        // mob.squashed.connect($UserInterface/ScoreLabel._on_mob_squashed.bind())
-        mob.connect("squashed", &self.user_interface.callable("on_mob_squashed"));
-
-        // add_child(mob)
+        // Spawn the mob by adding it to the Main scene.
         self.base_mut().add_child(&mob);
+
+        // We connect the mob to the score label to update the score upon squashing a mob.
+        mob.connect("squashed", &self.user_interface.callable("on_mob_squashed"));
     }
 
     #[func]
     pub fn on_player_hit(&mut self) {
-        // $MobTimer.stop()
         self.mob_timer.stop();
-        // $UserInterface/Retry.show()
+
         self.base()
             .get_node_as::<ColorRect>("UserInterface/Retry")
             .show();
