832f2bdf11d1be91c4382680d156de342735550b
Frank Casanova <frankcasanova.info@gmail.com>
2025-03-31 11:31:41 +0200

    refactor(mob): remove commented code and improve code clarity
    Removed unnecessary commented-out code to improve readability and maintainability. The changes do not alter the behavior of the code but make it cleaner and easier to understand.

--- squash-the-creeps/rust/src/mob.rs
+++ squash-the-creeps/rust/src/mob.rs
@@ -20,7 +20,6 @@ pub struct Mob {
 #[godot_api]
 impl ICharacterBody3D for Mob {
     fn physics_process(&mut self, _delta: f64) {
-        // move_and_slide()
         self.base_mut().move_and_slide();
     }
 }
@@ -28,19 +27,15 @@ impl ICharacterBody3D for Mob {
 impl Mob {
     #[func]
     pub fn initialize(&mut self, start_position: Vector3, player_position: Vector3) {
-        // look_at_from_position(start_position, player_position, Vector3.UP)
         self.base_mut()
             .look_at_from_position(start_position, player_position);
 
-        // rotate_y(randf_range(-PI / 4, PI / 4))
         self.base_mut()
             .rotate_y(rand::rng().random_range(-PI / 4.0..PI / 4.0));
 
-        // var random_speed = randf_range(min_speed, max_speed)
         let random_speed = rand::rng().random_range(self.min_speed..self.max_speed);
 
         // We calculate a forward velocity first, which represents the speed.
-        // velocity = Vector3.FORWARD * random_speed
         self.base_mut()
             .set_velocity(Vector3::FORWARD * random_speed);
 
@@ -48,33 +43,29 @@ impl Mob {
         let velocity = self.base().get_velocity();
 
         // We then rotate the vector based on the mob's Y rotation to move in the direction it's looking.
-        // velocity = velocity.rotated(Vector3.UP, rotation.y)
         self.base_mut()
             .set_velocity(velocity.rotated(Vector3::UP, rotation.y));
 
-        // $AnimationPlayer.speed_scale = random_speed / min_speed
         let animation_speed = rand::rng().random_range(1.0..6.0);
+
         self.base()
             .get_node_as::<AnimationPlayer>("AnimationPlayer")
             .set_speed_scale(animation_speed as f32)
     }
+
     // Emitted when the player jumped on the mob.
-    // signal squashed
     #[signal]
     pub fn squashed();
 
     #[func]
     pub fn squash(&mut self) {
-        // squashed.emit()
         self.signals().squashed().emit();
 
-        // queue_free()
         self.base_mut().queue_free();
     }
 
     #[func]
     fn on_visible_on_screen_notifier_3d_screen_exited(&mut self) {
-        // queue_free()
         self.base_mut().queue_free();
     }
 }
